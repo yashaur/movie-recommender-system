846cfe67206d5717a25fd9e3040f459da3a8e654
shaurya <work.shauryasingru@gmail.com>
2026-03-19 20:07:09 +0530

    Switching to html app architecture instead of Streamlit

---
+++ api/generate_poster.py
@@ -0,0 +1,45 @@
+import requests
+import time
+from io import BytesIO
+from PIL import Image
+
+def get_poster_url(movie_title):
+    
+    API_KEY = '46f52586'
+    BASE_URL = 'http://www.omdbapi.com'
+    POSTER_BASE_URL = 'http://img.omdbapi.com'
+
+
+    
+    try:
+        params_omdb = {
+        't': movie_title,
+        'apikey': API_KEY
+                }
+        response = requests.get(BASE_URL, params=params_omdb).json()
+            
+    except:
+        params_omdb = {
+        's': movie_title,
+        'apikey': API_KEY
+                }
+
+        response = requests.get(BASE_URL, params=params_omdb).json()['Search'][0]
+    
+    id = response['imdbID']
+    imdb_poster_url = response['Poster']
+        
+    try:
+        params_poster = {
+                            'i': id,
+                            'h': 350,
+                            'apikey': API_KEY
+            }
+
+        poster = Image.open(BytesIO(requests.get(POSTER_BASE_URL, params = params_poster).content))
+
+    except:
+        poster = Image.open(BytesIO(requests.get(imdb_poster_url).content))
+
+    finally:
+        return poster
---
+++ api/recommend.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import numpy as np
+from sklearn.preprocessing import normalize
+import joblib
+import pandas as pd
+from api.generate_poster import get_poster_url
+import base64, io
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+app = FastAPI(title = 'Movie Recommender')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+indices = joblib.load('./artifacts/indices.pkl')
+cosine_sim = joblib.load('./artifacts/cosine_sim.pkl')
+
+class Movie_input(BaseModel):
+    titles: list[str]
+
+class Recommendation_output(BaseModel):
+    recommendations: list
+
+class MovieListOutput(BaseModel):
+    titles: list[str]
+
+def get_recommendations(movie_titles):
+    
+    sim_scores = []
+    input_movie_indices = []
+
+    for title in movie_titles:
+        idx = indices.loc[title, 'index']
+        input_movie_indices.append(idx)
+        sim_scores.append(cosine_sim[idx])
+
+    weights = [1, 1, 1]
+
+    sim_scores = np.average(np.array(sim_scores), axis = 0, weights = weights)
+    
+    sim_scores = enumerate(normalize(sim_scores.reshape(1,-1), norm='l2').flatten())
+
+    sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
+
+    movie_indices = [i[0] for i in sim_scores if i[0] not in input_movie_indices][:10]
+
+    # return movie_indices
+
+    recos = indices.iloc[movie_indices].drop(columns = 'index').reset_index(names = 'title').T.to_dict()
+    recos = [recos[n] for n in range(10)]
+    
+    for idx, movie in enumerate([movie['title'] for movie in recos]):
+        poster_img = get_poster_url(movie)
+        if poster_img:
+            buf = io.BytesIO()
+            poster_img.save(buf, format="JPEG")
+            poster_img = base64.b64encode(buf.getvalue()).decode()
+        else:
+            with open('./artifacts/movie_poster_not_found.jpg', 'rb') as f:
+                poster_img = base64.b64encode(f.read()).decode()
+        
+        recos[idx]['poster'] = poster_img
+
+    return recos
+
+@app.get("/")
+async def serve_frontend():
+    return FileResponse("app.html")
+
+@app.get('/titles', response_model = MovieListOutput)
+async def get_titles():
+    return {'titles': indices.index.to_list()}
+
+
+@app.post('/recommend', response_model = Recommendation_output)
+async def recommend(payload: Movie_input):
+
+    movie_titles = payload.titles
+    recos = get_recommendations(movie_titles=movie_titles)
+
+    return {'recommendations': recos}
